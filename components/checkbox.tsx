f42b002af36930643dfbf20b17a69126b7204958
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-04-16 15:55:29 +0900

    Refactor Checkbox

--- components/checkbox.tsx
+++ components/checkbox.tsx
@@ -8,15 +8,44 @@ export default function Checkbox({
   ...props
 }: {} & ComponentPropsWithRef<"input">) {
   return (
-    <input
-      type="checkbox"
-      className={twMerge(
-        "h-[24px] w-[24px] appearance-none rounded-full border-1 border-none outline-1 -outline-offset-1 outline-label-assistive transition-colors disabled:cursor-not-allowed",
-        "checked:bg-primary-sub-1 checked:bg-[url(data:image/svg+xml,%3Csvg%20width%3D%2214%22%20height%3D%2211%22%20viewBox%3D%220%200%2014%2011%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22M1%205.84408L4.95599%209.80007L12.3028%200.757812%22%20stroke%3D%22white%22%20stroke-width%3D%221.5%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%2F%3E%3C%2Fsvg%3E)] checked:bg-center checked:bg-no-repeat checked:outline-0",
-        "disabled:bg-interaction-disable disabled:outline-line-neutral disabled:checked:bg-[url(data:image/svg+xml,%3Csvg%20width%3D%2214%22%20height%3D%2211%22%20viewBox%3D%220%200%2014%2011%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22M1%205.84408L4.95599%209.80007L12.3028%200.757812%22%20stroke%3D%22%23DBDCDF%22%20stroke-width%3D%221.5%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%2F%3E%3C%2Fsvg%3E)]",
-        className,
-      )}
-      {...props}
-    />
+    <label className="group inline-block has-disabled:cursor-not-allowed">
+      <input
+        type="checkbox"
+        className={twMerge("hidden", className)}
+        {...props}
+      />
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="0.4"
+          y="0.4"
+          width="15.2"
+          height="15.2"
+          rx="7.6"
+          className="fill-common-100 transition-[fill] group-has-checked:fill-primary-sub-1 group-has-disabled:fill-interaction-disable"
+        />
+        <rect
+          x="0.4"
+          y="0.4"
+          width="15.2"
+          height="15.2"
+          rx="7.6"
+          strokeWidth="0.8"
+          className="stroke-label-assistive transition-[stroke] group-has-checked:stroke-transparent group-has-disabled:stroke-line-neutral group-has-disabled:group-has-checked:stroke-transparent"
+        />
+        <path
+          d="M4 8.4001L6.8 11.2001L11.2 5.6001"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="stroke-transparent transition-[stroke] group-has-checked:stroke-common-100 group-has-disabled:group-has-checked:stroke-label-disable"
+        />
+      </svg>
+    </label>
   );
 }
