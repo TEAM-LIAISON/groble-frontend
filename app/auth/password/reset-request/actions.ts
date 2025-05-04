f20d8e82bc066903d38ea4855580224d07465b22
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-05-04 16:10:53 +0900

    Update Orval to version 7.9

--- app/auth/password/reset-request/actions.ts
+++ app/auth/password/reset-request/actions.ts
@@ -7,13 +7,9 @@ export async function requestPasswordResetAction(
   _: requestPasswordResetResponse | null,
   formData: FormData,
 ) {
-  const response = await requestPasswordReset(
-    {
-      email: formData.get("email") as string,
-    },
-    // @ts-expect-error
-    {},
-  );
+  const response = await requestPasswordReset({
+    email: formData.get("email") as string,
+  });
 
   if (response.status == 200) redirect("/auth/password/reset-request-complete");
   else return response;
