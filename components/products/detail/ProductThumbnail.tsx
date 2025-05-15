b2305bbfce355ca0562d85fe5658e36d174038e4
김동혁 <clfgnm9@naver.com>
2025-05-15 14:52:47 +0900

    feat: 상품 썸네일 컴포넌트 추가

---
+++ components/products/detail/ProductThumbnail.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+interface ProductHeaderProps {
+  thumbnailUrl: string;
+}
+
+export default function ProductThumbnail({ thumbnailUrl }: ProductHeaderProps) {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <div className="relative h-[251px] w-[335px] rounded-[0.75rem]">
+        <Image
+          src={thumbnailUrl}
+          alt="thumbnail"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+    </div>
+  );
+}
