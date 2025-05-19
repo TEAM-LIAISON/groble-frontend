da5ba1c5d3591d781cee845b9a51bfcf278ee1d2
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-05-19 20:18:20 +0900

    fix: apply navigation bar

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
 import { GrobleLogo } from "../icons/GrobleLogo";
+import NavigationBar from "../navigation-bar";
 
 export default function Footer() {
   const segments = useSelectedLayoutSegments();
@@ -58,6 +59,7 @@ export default function Footer() {
             © 2025. liaison. All rights reserved.
           </div>
         </div>
+        <NavigationBar />
       </footer>
     )
   );
