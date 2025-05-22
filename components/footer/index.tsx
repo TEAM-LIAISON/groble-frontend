a87dede1203b89c3623bef0622362fd630c5e4b5
김동혁 <clfgnm9@naver.com>
2025-05-22 14:46:56 +0900

    chore: Footer 정보 변경

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -21,35 +21,38 @@ export default function Footer() {
             </div>
 
             <div className="flex flex-col gap-1 text-caption-1 text-label-alternative">
-              <p>대표자: 주서영</p>
-              <p>개인정보관리책임자: 권동민</p>
-              <p>주소: 서울특별시 마포구 와우산로 94</p>
+              <div className="flex gap-1">
+                <p>상호명:리에종 |</p>
+                <p>대표자: 주서영 |</p>
+                <p>사업자등록번호 : 515-36-92976</p>
+              </div>
+              <p>주소: 서울 광진구 광나루로19길 23, 103호</p>
             </div>
 
             <div className="mt-3 mb-8 flex flex-wrap gap-[1.12rem] text-sm">
               <Link
-                href="/privacy"
+                href="https://paint-crowley-ff2.notion.site/1f2c158365ac809bb1cbc7723ac4b346?pvs=4"
                 className="text-caption-1 text-label-neutral hover:text-label-normal"
               >
                 개인정보 처리방침
               </Link>
               <Link
-                href="/terms"
+                href="https://paint-crowley-ff2.notion.site/1f2c158365ac80c39fc3ef1b8764f53a?pvs=4"
                 className="text-caption-1 text-label-neutral hover:text-label-normal"
               >
                 서비스 이용약관
               </Link>
               <Link
-                href="/marketing"
+                href="https://paint-crowley-ff2.notion.site/1f2c158365ac80afafe6c9d7c1011d39?pvs=4"
                 className="text-caption-1 text-label-neutral hover:text-label-normal"
               >
-                메이커 이용 약관
+                메이커 이용약관
               </Link>
               <Link
-                href="/refund"
+                href="https://paint-crowley-ff2.notion.site/1f2c158365ac80328c6fde9ceaf77ec6?pvs=4"
                 className="text-caption-1 text-label-neutral hover:text-label-normal"
               >
-                취소 및 환불 규정
+                환불 규정
               </Link>
             </div>
           </div>
