b0b8975662ce776065c50a080aa17f8a3ffb15fe
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-05-26 21:05:57 +0900

    feat: pass email to ResetPasswordRequestForm

--- app/reset-password-request/page.tsx
+++ app/reset-password-request/page.tsx
@@ -1,4 +1,5 @@
 import Header, { Back } from "@/components/header";
+import { getUserMyPageDetail } from "@/lib/api";
 import { Metadata } from "next";
 import ResetPasswordRequestForm from "./form";
 
@@ -6,13 +7,21 @@ export const metadata: Metadata = {
   title: "비밀번호 재설정 요청",
 };
 
-export default function ResetPasswordRequestPage() {
+export default async function ResetPasswordRequestPage() {
+  const response = await getUserMyPageDetail(
+    // @ts-expect-error
+    {},
+  );
+
+  let email;
+  if (response.status == 200) email = response.data.data?.email;
+
   return (
     <div className="flex flex-col bg-background-normal md:items-center md:justify-center">
       <div className="w-full md:mt-[150px] md:max-w-[480px]">
         <Header left={<Back />} />
         <main className="flex flex-col gap-8 p-5">
-          <ResetPasswordRequestForm />
+          <ResetPasswordRequestForm email={email} />
         </main>
       </div>
     </div>
