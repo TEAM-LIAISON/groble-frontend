6fce1a463a83c2842b519a125e0c454bf4f24406
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-05-26 21:36:36 +0900

    feat: enhance AgreeToTermsForm with additional reason options for account deletion

--- app/users/me/delete-account/form.tsx
+++ app/users/me/delete-account/form.tsx
@@ -7,13 +7,20 @@ import { useToastErrorMessage } from "@/lib/error";
 import { useUserStore } from "@/lib/store/useUserStore";
 import { twMerge } from "@/lib/tailwind-merge";
 import Form from "next/form";
-import { ReactNode, startTransition, useActionState, useEffect } from "react";
+import {
+  ReactNode,
+  startTransition,
+  useActionState,
+  useEffect,
+  useState,
+} from "react";
 import { logoutAfterDeleteAccountAction } from "../settings/actions";
 import { withdrawUserAction } from "./actions";
 
 export default function AgreeToTermsForm() {
   const [response, formAction] = useActionState(withdrawUserAction, null);
   const userStore = useUserStore();
+  const [hasReason, setHasReason] = useState(false);
 
   useToastErrorMessage(response);
   useEffect(() => {
@@ -42,25 +49,50 @@ export default function AgreeToTermsForm() {
         </p>
       </div>
       <div className="flex flex-col gap-2">
-        <Item name="reason" value="NOT_USING">
+        <Item
+          name="reason"
+          value="NOT_USING"
+          onClick={() => setHasReason(false)}
+        >
           서비스를 잘 이용하지 않아요
         </Item>
-        <Item name="reason" value="INCONVENIENT">
+        <Item
+          name="reason"
+          value="INCONVENIENT"
+          onClick={() => setHasReason(true)}
+        >
           서비스 이용이 불편해요
         </Item>
-        <Item name="reason" value="LACKS_CONTENT">
+        <Item
+          name="reason"
+          value="LACKS_CONTENT"
+          onClick={() => setHasReason(true)}
+        >
           필요한 기능이나 콘텐츠가 없어요
         </Item>
-        <Item name="reason" value="BAD_EXPERIENCE">
+        <Item
+          name="reason"
+          value="BAD_EXPERIENCE"
+          onClick={() => setHasReason(true)}
+        >
           불쾌한 경험을 겪었어요
         </Item>
-        <Item name="reason" value="COST_BURDEN">
+        <Item
+          name="reason"
+          value="COST_BURDEN"
+          onClick={() => setHasReason(false)}
+        >
           가격 및 비용이 부담돼요
         </Item>
-        <Item name="reason" value="OTHER">
+        <Item name="reason" value="OTHER" onClick={() => setHasReason(true)}>
           기타
         </Item>
-        <TextAreaTextField name="reason" placeholder="상세 사유를 적어주세요" />
+        <TextAreaTextField
+          hidden={!hasReason}
+          name="reason"
+          placeholder="상세 사유를 적어주세요"
+          rows={6}
+        />
       </div>
       <BottomArea narrow>
         <BottomButton>탈퇴하기</BottomButton>
@@ -73,11 +105,13 @@ function Item({
   name,
   value,
   className,
+  onClick,
   children,
 }: {
   name?: string;
   value?: string;
   className?: string;
+  onClick?: () => void;
   children?: ReactNode;
 }) {
   return (
@@ -86,6 +120,7 @@ function Item({
         "flex items-center gap-2 py-2 text-body-2-normal font-medium",
         className,
       )}
+      onClick={onClick}
     >
       <Radio name={name} value={value} />
       <span className="flex-1">{children}</span>
