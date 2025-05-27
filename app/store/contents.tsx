f6f076a85b1f4a22a44822e5df7abfd0cea2f509
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-05-27 20:53:23 +0900

    fix: update toast messages for content action success feedback

--- app/store/contents.tsx
+++ app/store/contents.tsx
@@ -434,6 +434,8 @@ function Content({
               startTransition(async () => {
                 const response = await activeContentAction(item.contentId!);
                 toastErrorMessage(response);
+
+                if (response.status == 200) toast("판매가 시작되었습니다.");
               })
             }
           >
@@ -471,7 +473,7 @@ function Content({
                   const response = await stopContentAction(item.contentId!);
                   toastErrorMessage(response);
 
-                  if (response.status == 200) toast("판매가 시작되었습니다.");
+                  if (response.status == 200) toast("판매가 중단되었습니다.");
                 })
               }
             >
