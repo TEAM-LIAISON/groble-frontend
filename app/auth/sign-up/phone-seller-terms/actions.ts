9cbc1702ddd402f5fe75c30f39f0d06217fdd478
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-05-29 09:08:47 +0900

    fix: simplify parameter type in setPhoneSellerTermsAction function

--- app/auth/sign-up/phone-seller-terms/actions.ts
+++ app/auth/sign-up/phone-seller-terms/actions.ts
@@ -1,14 +1,10 @@
 "use server";
 
-import {
-  agreeMakerTermsResponse400,
-  getUserMyPageDetailResponse401,
-} from "@/lib/api";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function setPhoneSellerTermsAction(
-  _: getUserMyPageDetailResponse401 | agreeMakerTermsResponse400 | null,
+  _: void | null,
   formData: FormData,
 ) {
   (await cookies()).set("Maker-Terms-Agreement", "true");
