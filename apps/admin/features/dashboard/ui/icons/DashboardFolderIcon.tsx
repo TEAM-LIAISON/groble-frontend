06c2aa2ea701469d8d3565d73a026bf36be15577
김동혁 <clfgnm9@naver.com>
2025-06-14 18:59:38 +0900

    feat: 대시보드 레이아웃 및 사이드바 컴포넌트 추가, 아이콘 컴포넌트 생성

---
+++ apps/admin/features/dashboard/ui/icons/DashboardFolderIcon.tsx
@@ -0,0 +1,21 @@
+// features/dashboard/ui/icons/DashboardHomeIcon.tsx
+import React from 'react';
+
+export default function DashboardFolderIcon(
+  props: React.SVGProps<SVGSVGElement>
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M3 20C2.44772 20 2 19.5523 2 19V5C2 4.44772 2.44772 4 3 4H9.46482C9.79917 4 10.1114 4.1671 10.2969 4.4453L11.7031 6.5547C11.8886 6.8329 12.2008 7 12.5352 7H21C21.5523 7 22 7.44772 22 8V19C22 19.5523 21.5523 20 21 20H3Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
