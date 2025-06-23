33a6c7d7c2462408737a33d0ebf715681fa69f1c
김동혁 <clfgnm9@naver.com>
2025-06-23 20:25:27 +0900

    feat: 판매자 프로필 사이드바에 메이커 인증 안내 링크 추가 및 프로필 메뉴 간격 조정

--- apps/web/features/profile/ui/ProfileSidebar.tsx
+++ apps/web/features/profile/ui/ProfileSidebar.tsx
@@ -6,6 +6,9 @@ import { UserProfileCard } from './UserProfileCard';
 import { ProfileMenuList } from './ProfileMenuList';
 import { profileMenuGroups, sellerProfileMenuGroups } from '../config/menuData';
 import type { ProfileMenuGroup } from '../model/types';
+import { ChevronIcon } from '@/components/(improvement)/icons';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 export function ProfileSidebar() {
   const { data: userResponse, isLoading } = useUserDetail();
@@ -50,6 +53,28 @@ export function ProfileSidebar() {
       {/* 사용자 프로필 카드 */}
       {userResponse?.data && (
         <UserProfileCard userDetail={userResponse.data} isLoading={isLoading} />
+      )}
+
+      {/* 메이커 인증 말풍선 표시 */}
+
+      {userResponse?.data?.userType === 'SELLER' &&
+      userResponse?.data?.verificationStatus === 'PENDING' ? (
+        <Link
+          href="/users/maker/select-type"
+          className="mt-8 py-5 px-4 flex justify-between items-center bg-[#FEECEC] rounded-xl border-dashed border-[1.5px] border-status-error cursor-pointer hover:brightness-[102%]"
+        >
+          <div className="flex flex-col gap-[0.12rem]">
+            <p className="text-body-1-normal font-semibold text-label-normal">
+              정산을 받으려면 메이커 인증이 필요해요
+            </p>
+            <p className="text-body-1-normal text-status-error font-semibold ">
+              인증하러 가기
+            </p>
+          </div>
+          <ChevronIcon className="w-5 h-5 text-status-error" />
+        </Link>
+      ) : (
+        <div className="mt-6"></div>
       )}
 
       {/* 메뉴 리스트 */}
