33f68f129df7300b501b495e12dd454fb4456477
김동혁 <clfgnm9@naver.com>
2025-06-23 16:49:48 +0900

    feat: 프로필 정보 목록 및 항목 컴포넌트 추가, 이메일 아이콘 컴포넌트 생성

--- apps/web/app/users/profile/page.tsx
+++ apps/web/app/users/profile/page.tsx
@@ -1,6 +1,10 @@
 'use client';
 
-import { useUserDetail, ProfileImageUpload } from '@/features/profile';
+import {
+  useUserDetail,
+  ProfileImageUpload,
+  ProfileInfoList,
+} from '@/features/profile';
 
 export default function ProfilePage() {
   const { data: userResponse, isLoading } = useUserDetail();
@@ -10,6 +14,10 @@ export default function ProfilePage() {
       <ProfileImageUpload
         profileImageUrl={userResponse?.data?.profileImageUrl}
       />
+
+      <div className="mt-[0.77rem]"></div>
+
+      <ProfileInfoList userData={userResponse?.data} />
     </div>
   );
 }
--- apps/web/features/profile/index.ts
+++ apps/web/features/profile/index.ts
@@ -3,6 +3,8 @@ export { ProfileSidebar } from './ui/ProfileSidebar';
 export { UserProfileCard } from './ui/UserProfileCard';
 export { ProfileMenuList } from './ui/ProfileMenuList';
 export { default as ProfileImageUpload } from './ui/ProfileImageUpload';
+export { default as ProfileInfoItem } from './ui/ProfileInfoItem';
+export { default as ProfileInfoList } from './ui/ProfileInfoList';
 
 // Models
 export type {
