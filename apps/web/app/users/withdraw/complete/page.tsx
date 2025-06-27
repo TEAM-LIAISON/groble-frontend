2dc09c194a86bec4a654be63873203e9a19c0a23
김동혁 <clfgnm9@naver.com>
2025-06-27 19:15:50 +0900

    feat: 탈퇴 페이지 UI 개선 및 버튼 비활성화 조건 수정

--- apps/web/app/users/withdraw/complete/page.tsx
+++ apps/web/app/users/withdraw/complete/page.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image';
 export default function WithdrawCompletePage() {
   return (
     <>
-      <WebHeader />
+      <WebHeader mobileBack="back" />
       <div className="w-full flex justify-center h-[calc(100vh-68px)]">
-        <div className="flex flex-col items-center max-w-[480px] w-full">
+        <div className="flex flex-col items-center max-w-[480px] w-full p-5 md:p-0">
           {/* 완료 메시지 */}
-          <div className="flex flex-col items-center mt-[9.06rem]">
+          <div className="flex flex-col items-center mt-[4rem] md:mt-[9.06rem]">
             <Image
               src="/images/groble-3d-1.svg"
               alt="logo"
@@ -19,7 +19,7 @@ export default function WithdrawCompletePage() {
             <h1 className="text-title-3 font-bold text-label-normal mt-8">
               탈퇴가 완료됐어요
             </h1>
-            <p className="mt-2 text-body-1-normal text-label-alternative leading-6 tracking-[0.00569em] text-center">
+            <p className="mt-2 text-body-2-normal md:text-body-1-normal text-label-alternative leading-6 tracking-[0.00569em] text-center">
               더 발전된 모습으로 여기 있을게요.
               <br />
               다시 찾아와주세요!
@@ -27,7 +27,7 @@ export default function WithdrawCompletePage() {
           </div>
 
           {/* 홈으로 가기 버튼 */}
-          <div className="w-full mt-auto mb-10">
+          <div className="w-full mt-auto mb-5">
             <LinkButton
               href="/"
               group="solid"
