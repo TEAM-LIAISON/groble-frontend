b5ef128ac209b0ea043090320b69234b22bede51
김동혁 <clfgnm9@naver.com>
2025-07-02 15:19:29 +0900

    feat: Intro 페이지에 IntroContentSection2 추가 및 아이콘 컴포넌트 통합

--- apps/web/app/intro/page.tsx
+++ apps/web/app/intro/page.tsx
@@ -1,16 +1,20 @@
 import WebHeader from '@/components/(improvement)/layout/header';
 import { IntroContentSection1, IntroHeroSection } from '@/features/intro';
+import IntroContentSection2 from '@/features/intro/components/intro-content-2';
 
 export default function IntroPage() {
   return (
     <>
       <WebHeader />
 
-      <div className="flex flex-col gap-[3.5rem] px-5">
+      <div className="flex flex-col gap-[3.5rem] ">
         <div className="w-full flex justify-center">
-          <div className="w-[67.5rem] flex flex-col py-[3.5rem] gap-[3.5rem]">
+          <div className="w-[67.5rem] flex flex-col py-[3.5rem] gap-[3.5rem] px-5">
             <IntroHeroSection />
+            {/* 그로블은 메이커를 위한 서비스입니다. */}
             <IntroContentSection1 />
+            {/* 이런 메이커를 찾고 있어요 */}
+            <IntroContentSection2 />
           </div>
         </div>
       </div>
--- apps/web/features/intro/components/index.ts
+++ apps/web/features/intro/components/index.ts
@@ -1,2 +1,3 @@
 export { default as IntroHeroSection } from './intro-hero-section';
 export { default as IntroContentSection1 } from './intro-content-1';
+export { default as IntroContentSection2 } from './intro-content-2';
