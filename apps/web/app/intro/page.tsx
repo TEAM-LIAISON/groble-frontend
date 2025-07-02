a215650dec513785865787964dfe8662056fbabb
김동혁 <clfgnm9@naver.com>
2025-07-02 14:49:38 +0900

    feat: Intro 페이지에 IntroContentSection1 추가 및 레이아웃 개선

--- apps/web/app/intro/page.tsx
+++ apps/web/app/intro/page.tsx
@@ -1,12 +1,18 @@
 import WebHeader from '@/components/(improvement)/layout/header';
-import { IntroHeroSection } from '@/features/intro';
+import { IntroContentSection1, IntroHeroSection } from '@/features/intro';
 
 export default function IntroPage() {
   return (
     <>
       <WebHeader />
+
       <div className="flex flex-col gap-[3.5rem] px-5">
-        <IntroHeroSection />
+        <div className="w-full flex justify-center">
+          <div className="w-[67.5rem] flex flex-col py-[3.5rem] gap-[3.5rem]">
+            <IntroHeroSection />
+            <IntroContentSection1 />
+          </div>
+        </div>
       </div>
     </>
   );
--- apps/web/features/intro/components/index.ts
+++ apps/web/features/intro/components/index.ts
@@ -1,1 +1,2 @@
 export { default as IntroHeroSection } from './intro-hero-section';
+export { default as IntroContentSection1 } from './intro-content-1';
