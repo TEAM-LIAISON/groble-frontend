49b032fd57750e2dd1ff918cc855d4cecb47fbc4
김동혁 <clfgnm9@naver.com>
2025-07-03 16:38:34 +0900

    feat: 스토어 관리 레이아웃 및 사이드바 컴포넌트 추가, 아이콘 컴포넌트 통합

---
+++ apps/web/app/manage/store/layout.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import WebHeader from '@/components/(improvement)/layout/header';
+import StoreSidebar from './components/StoreSidebar';
+
+/**
+ * 스토어 관리 레이아웃 컴포넌트
+ * @param children - 페이지 컨텐츠
+ */
+export default function StoreLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {/* 웹 헤더 (모바일 전용) */}
+      <WebHeader mobileTitle="스토어 관리" />
+
+      {/* 메인 레이아웃 컨테이너 */}
+      <div className={`h-[calc(100vh-66px)] bg-background-alternative flex `}>
+        {/* 왼쪽: 고정 사이드바 */}
+        <StoreSidebar />
+
+        {/* 오른쪽: 스크롤 가능한 메인 콘텐츠 영역 */}
+        <main className="flex-1 ml-60 overflow-y-auto ">
+          {/* 컨텐츠 래퍼 */}
+          <div className="">{children}</div>
+        </main>
+      </div>
+    </>
+  );
+}
--- apps/web/components/(improvement)/icons/index.ts
+++ apps/web/components/(improvement)/icons/index.ts
@@ -1,2 +1,10 @@
-export { GrobleLogo } from "./GrobleLogo";
-export { ChevronIcon } from "./ChevronIcon";
+export { GrobleLogo } from './GrobleLogo';
+export { ChevronIcon } from './ChevronIcon';
+
+// 사이드바 아이콘들
+export { DashboardIcon } from './DashboardIcon';
+export { StoreIcon } from './StoreIcon';
+export { BoxIcon } from './BoxIcon';
+export { WalletIcon } from './WalletIcon';
+export { CustomerIcon } from './CustomerIcon';
+export { SidebarLogoutIcon } from './SidebarLogoutIcon';
