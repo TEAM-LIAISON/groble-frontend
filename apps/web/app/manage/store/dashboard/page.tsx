84e92e40dca39b94c205da7212b21f1fb51900fe
김동혁 <clfgnm9@naver.com>
2025-08-01 16:02:52 +0900

    feat: ComingSoon 컴포넌트에 서브타이틀 추가 및 관련 페이지 업데이트

--- apps/web/app/manage/store/dashboard/page.tsx
+++ apps/web/app/manage/store/dashboard/page.tsx
@@ -10,6 +10,7 @@ export default function DashboardPage() {
       <div className="mt-16 flex flex-col justify-center md:justify-start mx-auto rounded-xl bg-white  md:px-9 md:py-12 py-5 shadow-card min-h-[calc(100vh-122px)]">
         <ComingSoon
           title="대시보드"
+          subTitle="스토어 운영 현황을 한 눈에 확인하세요"
           description="총 수익, 조회수, 고객수 등을\n 확인할 수 있어요."
         />
       </div>
