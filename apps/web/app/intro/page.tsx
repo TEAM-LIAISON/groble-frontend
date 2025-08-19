126f74c6363d0a2f5214bee79a45a5d8b6995ff7
김동혁 <clfgnm9@naver.com>
2025-08-20 02:41:03 +0900

    feat: IntroPage 및 FAQ 섹션 내용 수정

--- apps/web/app/intro/page.tsx
+++ apps/web/app/intro/page.tsx
@@ -34,19 +34,19 @@ export default function IntroPage() {
             {/* 이런 메이커를 찾고 있어요 */}
             <IntroContentSection2 />
 
-            {/* 그로블 서비스 흐름 */}
+            {/* 스토어 운영 현황을 한 눈에 확인해요 */}
             <IntroContentSection3 />
 
-            {/* 얼리메이커를 위한 혜택 */}
+            {/* 나의 브랜드 마켓을 생성하고 관리해요 */}
             <IntroContentSection4 />
 
-            {/* 콘텐츠 종류별 판매가이드 */}
+            {/* 콘텐츠를 등록하고 자유롭게 판매해요 */}
             <IntroContentSection5 />
 
-            {/* 콘텐츠 종류별 판매가이드 */}
+            {/* 나의 판매 데이터와 리뷰를 관리해요 */}
             <IntroContentSection6 />
 
-            {/* 콘텐츠 종류별 판매가이드 */}
+            {/* 정산 현황을 한 눈에 확인해요 */}
             <IntroContentSection7 />
 
             {/* 콘텐츠 종류별 판매가이드 */}
