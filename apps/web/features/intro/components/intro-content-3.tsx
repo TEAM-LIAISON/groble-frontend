e9c3abb86345d449434a1b385f30db2f3fd37da0
김동혁 <clfgnm9@naver.com>
2025-08-20 02:22:20 +0900

    feat: IntroContentSection4 컴포넌트 수정 및 이미지 파일 추가

--- apps/web/features/intro/components/intro-content-3.tsx
+++ apps/web/features/intro/components/intro-content-3.tsx
@@ -1,27 +1,29 @@
 import Image from 'next/image';
-import { ArrowIcon } from '../assets';
 
 export default function IntroContentSection3() {
   return (
-    <div className="flex flex-col">
-      <div>
-        <span className="rounded-sm bg-[#D8FFF4] px-3 py-2 text-primary-sub-1">
-          대시보드
-        </span>
+    <div className="flex flex-col md:items-start items-center">
+      <div className="flex flex-col md:items-start items-center max-w-[14.5rem] md:max-w-none">
+        <div>
+          <span className="text-body-1-normal font-semibold rounded-sm bg-[#D8FFF4] px-3 py-2 text-primary-sub-1">
+            대시보드
+          </span>
+        </div>
+        <h2 className="text-center text-title-3 md:text-title-2 font-bold text-label-normal relative mt-5">
+          스토어 운영 현황을
+          <br className="md:hidden block" /> 한 눈에 확인하세요.
+        </h2>
+        <p className="text-body-2-normal md:text-heading-1 text-label-alternative mt-1 text-center ">
+          총 수익, 조회수, 고객수, 유입 경로 등을 확인할 수 있어요.
+        </p>
       </div>
-      <h2 className="text-title-3 md:text-title-2 font-bold text-label-normal relative mt-5">
-        스토어 현황을 한 눈에 확인해요.
-      </h2>
-      <p className="text-heading-1 text-label-alternative mt-1">
-        총 수익, 조회수, 고객수, 유입 경로 등을 확인할 수 있어요.
-      </p>
 
       <Image
         src="/images/intro/section-3-ui-pc.svg"
         alt="section-3-ui-pc"
         width={1040}
         height={511}
-        className="mt-6 md:block hidden"
+        className="mt-6 md:block hidden scale-[1.03]"
       />
 
       <div className="w-full mt-5 md:hidden block">
@@ -31,7 +33,7 @@ export default function IntroContentSection3() {
           width={0}
           height={0}
           sizes="100vw"
-          className="w-full h-auto"
+          className="w-full h-auto scale-[1.03]"
         />
       </div>
     </div>
--- apps/web/features/intro/components/intro-content-4.tsx
+++ apps/web/features/intro/components/intro-content-4.tsx
@@ -1,157 +1,38 @@
 import Image from 'next/image';
-import { TripleArrowIcon } from '../assets';
 
 export default function IntroContentSection4() {
   return (
-    <div className="flex flex-col">
-      <h2 className="text-title-3 md:text-title-2 font-bold text-label-normal relative">
-        얼리메이커를 위한 혜택
-      </h2>
-
-      <div className="mt-5 bg-background-alternative rounded-xl py-9 px-5 md:px-8 flex flex-col">
-        <span className="text-heading-1 text-primary-sub-2">#1</span>
-        <h3 className="text-title-3 font-bold text-label-normal">
-          판매 수수료 인하
-        </h3>
-        <p className="mt-1 tracking-[0.01888rem] text-caption-1 text-label-alternative">
-          <span className="text-body-2-normal text-label-neutral">
-            얼리 메이커 대상 3개월간 1.5% 수수료 고정 (업계 평균 10~20%)
-          </span>
-          <br /> *추후 인상건에 대해서는 사전에 안내드릴 예정입니다.
-        </p>
-        <div className="flex flex-col md:flex-row gap-[1.12rem] md:gap-[3.44rem] items-center justify-center py-9">
-          <div className="relative md:w-[294px] md:h-[148px] w-[295px] h-[102px]">
-            <Image src="/images/intro/section4_1.svg" alt="section4_1" fill />
-          </div>
-          <TripleArrowIcon direction="right" className="md:block hidden" />
-          <TripleArrowIcon
-            direction="down"
-            className="md:hidden block"
-            width={40}
-            height={30}
-          />
-
-          <Image
-            src="/images/intro/section4_2.svg"
-            alt="section4_2"
-            width={295}
-            height={282}
-            className="md:block hidden"
-          />
-          <Image
-            src="/images/intro/section4_2_1.svg"
-            alt="section4_2_1"
-            width={271}
-            height={103}
-            className="md:hidden block"
-          />
-        </div>
+    <div className="flex flex-col md:items-start items-center">
+      <div className="flex flex-col md:items-start items-center max-w-[14.5rem] md:max-w-none">
+        <span className="text-body-1-normal font-semibold rounded-sm bg-[#D8FFF4] px-3 py-2 text-primary-sub-1">
+          마켓 관리
+        </span>
+        <h2 className="text-center text-title-3 md:text-title-2 font-bold text-label-normal relative mt-5">
+          나의 브랜드 마켓을
+          <br className="md:hidden block" /> 생성하고 관리해요
+        </h2>
       </div>
+      <p className="text-body-2-normal md:text-heading-1 text-label-alternative mt-1 text-center">
+        마켓명, 로고, URL, 문의 수단을 설정할 수 있어요
+      </p>
 
-      {/* 2,3 */}
-      <div className="flex flex-col md:flex-row mt-3 gap-3 w-full">
-        {/* 2 */}
-        <div className="bg-background-alternative py-9 px-5 md:px-8 w-full rounded-xl">
-          <span className="text-heading-1 text-primary-sub-2">#2</span>
-          <h2 className="text-title-3 md:text-title-2 font-bold text-label-normal relative">
-            CRM 기능 무료 지원
-          </h2>
-          <p className="text-body-2-normal text-label-neutral">
-            일회성 거래로 끝나지 않도록 고객 관리 지원
-          </p>
-          <div className="mt-7 flex flex-col gap-2">
-            {/* 1-1 */}
-            <div
-              className="bg-white rounded-lg p-4 flex gap-2 items-center "
-              style={{
-                boxShadow: '0px 2px 8px 0px rgba(0, 0, 0, 0.05)',
-              }}
-            >
-              <Image
-                src="/images/intro/chart-3d.svg"
-                alt="section4_3"
-                width={72}
-                height={72}
-              />
-              <div className="flex flex-col gap-1">
-                <p className="text-body-1-normal font-bold text-label-normal">
-                  자동화 메시지
-                </p>
-                <p className="text-label-1-normal text-label-neutral">
-                  고객 행동에 맞춰 메시지와
-                  <br className="md:hidden" /> 쿠폰 자동으로 발송
-                </p>
-              </div>
-            </div>
-            {/* 1-2 */}
-            <div
-              className="bg-white rounded-lg p-4 flex gap-2 items-center "
-              style={{
-                boxShadow: '0px 2px 8px 0px rgba(0, 0, 0, 0.05)',
-              }}
-            >
-              <Image
-                src="/images/intro/bell-3d.svg"
-                alt="section4_3"
-                width={72}
-                height={72}
-              />
-              <div className="flex flex-col gap-1">
-                <p className="text-body-1-normal font-bold text-label-normal">
-                  그룹 알림 발송
-                </p>
-                <p className="text-label-1-normal text-label-neutral">
-                  지정된 고객 그룹에게 메시지와 <br className="md:hidden" />
-                  쿠폰 발송
-                </p>
-              </div>
-            </div>
+      <Image
+        src="/images/intro/section-4-ui-pc.svg"
+        alt="section-4-ui-pc"
+        width={1040}
+        height={817}
+        className="mt-6 md:block hidden scale-[1.03]"
+      />
 
-            {/* 1-3 */}
-            <div
-              className="bg-white rounded-lg p-4 flex gap-2 items-center "
-              style={{
-                boxShadow: '0px 2px 8px 0px rgba(0, 0, 0, 0.05)',
-              }}
-            >
-              <Image
-                src="/images/intro/coupon-3d.svg"
-                alt="section4_3"
-                width={72}
-                height={72}
-              />
-              <div className="flex flex-col gap-1">
-                <p className="text-body-1-normal font-bold text-label-normal">
-                  맞춤 쿠폰 생성
-                </p>
-                <p className="text-label-1-normal text-label-neutral">
-                  할인 방식과 조건을 설정해
-                  <br className="md:hidden" />
-                  특정 상품 또는 전체 상품에 적용
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* 3 */}
-        <div className="bg-background-alternative py-9 px-5 md:px-8 w-full rounded-xl">
-          <span className="text-heading-1 text-primary-sub-2">#3</span>
-          <h2 className="text-title-3 md:text-title-2 font-bold text-label-normal relative">
-            대시보드 제공
-          </h2>
-          <p className="text-body-2-normal text-label-neutral">
-            수익, 조회수, 고객 흐름을 한눈에 볼 수 있는 맞춤형 대시보드 제공
-          </p>
-
-          <Image
-            src="/images/intro/dashboard.svg"
-            alt="dashboard-3d"
-            width={431}
-            height={307}
-            className="mt-7"
-          />
-        </div>
+      <div className="w-full mt-5 md:hidden block">
+        <Image
+          src="/images/intro/section-4-ui-mobile.svg"
+          alt="section-4-ui-mobile"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-auto scale-[1.03]"
+        />
       </div>
     </div>
   );
