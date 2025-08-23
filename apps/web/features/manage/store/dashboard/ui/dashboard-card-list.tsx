676a31751e060dfbd9c03ca98a16709342f41ba9
김동혁 <clfgnm9@naver.com>
2025-08-23 15:30:06 +0900

    feat: 대시보드 카드 리스트에 메이커 인증 말풍선 추가
    - 대시보드 카드 리스트 컴포넌트에 메이커 인증 상태에 따라 MakerCertificationBubble 컴포넌트를 조건부로 렌더링하도록 수정
    - UI 구조를 개선하여 가독성을 높임

--- apps/web/features/manage/store/dashboard/ui/dashboard-card-list.tsx
+++ apps/web/features/manage/store/dashboard/ui/dashboard-card-list.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import { getDashboardOverview } from '../api/get-dashboard-data';
 import LoadingSpinner from '@/shared/ui/LoadingSpinner';
+import MakerCertficationBubble from '@/entities/maker/ui/maker-certfication-bubble';
 
 export default function DashboardCardList() {
   const { data, isLoading } = useQuery({
@@ -22,129 +23,132 @@ export default function DashboardCardList() {
   }
 
   return (
-    <div className="grid md:grid-cols-4 grid-cols-2 gap-3">
-      {/* 카드 1 */}
-      <div className="p-6 rounded-xl bg-background-alternative flex flex-col">
-        <p className="md:text-body-1-normal text-body-2-normal font-semibold text-label-normal">
-          총 수익
-        </p>
-        <hr className="my-3 border-line-normal" />
-
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
-          수익
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {Math.floor(overview?.totalRevenue ?? 0).toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            원
-          </span>
-        </p>
-
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
-          건수
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {/* 소수점 내림 처리 */}
-          {Math.floor(overview?.totalSalesCount ?? 0).toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            건
-          </span>
-        </p>
-      </div>
-
-      {/* 카드 2 */}
-      <div className="p-6 rounded-xl bg-background-alternative flex flex-col">
-        <p className="md:text-body-1-normal text-body-2-normal font-semibold text-label-normal">
-          N월 수익
-        </p>
-        <hr className="my-3 border-line-normal" />
-
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
-          수익
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {Math.floor(overview?.currentMonthRevenue ?? 0).toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            원
-          </span>
-        </p>
-
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
-          건수
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {overview?.currentMonthSalesCount.toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            건
-          </span>
-        </p>
-      </div>
-
-      {/* 카드 3 */}
-      <Link
-        href="/"
-        className="p-6 rounded-xl bg-background-alternative flex flex-col"
-      >
-        <span className="flex justify-between">
+    <>
+      {overview?.verificationStatus && <MakerCertficationBubble />}
+      <div className="grid md:grid-cols-4 grid-cols-2 gap-3">
+        {/* 카드 1 */}
+        <div className="p-6 rounded-xl bg-background-alternative flex flex-col">
           <p className="md:text-body-1-normal text-body-2-normal font-semibold text-label-normal">
-            조회수
+            총 수익
           </p>
-          <ChevronIcon className="text-label-assistive" />
-        </span>
-        <hr className="my-3 border-line-normal" />
+          <hr className="my-3 border-line-normal" />
 
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
-          마켓
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {overview?.totalMarketViews.toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            회
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
+            수익
           </span>
-        </p>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {Math.floor(overview?.totalRevenue ?? 0).toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              원
+            </span>
+          </p>
 
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
-          콘텐츠
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {overview?.totalContentViews.toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            회
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
+            건수
           </span>
-        </p>
-      </Link>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {/* 소수점 내림 처리 */}
+            {Math.floor(overview?.totalSalesCount ?? 0).toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              건
+            </span>
+          </p>
+        </div>
 
-      {/* 카드 4 */}
-      <div className="p-6 rounded-xl bg-background-alternative flex flex-col">
-        <span className="flex justify-between">
+        {/* 카드 2 */}
+        <div className="p-6 rounded-xl bg-background-alternative flex flex-col">
           <p className="md:text-body-1-normal text-body-2-normal font-semibold text-label-normal">
-            고객 수
+            N월 수익
           </p>
-          {/* 추후 추가 */}
-          {/* <ChevronIcon className="text-label-assistive" /> */}
-        </span>
-        <hr className="my-3 border-line-normal" />
+          <hr className="my-3 border-line-normal" />
 
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
-          전체
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {overview?.totalCustomers.toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            명
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
+            수익
           </span>
-        </p>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {Math.floor(overview?.currentMonthRevenue ?? 0).toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              원
+            </span>
+          </p>
 
-        <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
-          신규
-        </span>
-        <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
-          {overview?.recentCustomers.toLocaleString()}
-          <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
-            명
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
+            건수
           </span>
-        </p>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {overview?.currentMonthSalesCount.toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              건
+            </span>
+          </p>
+        </div>
+
+        {/* 카드 3 */}
+        <Link
+          href="/"
+          className="p-6 rounded-xl bg-background-alternative flex flex-col"
+        >
+          <span className="flex justify-between">
+            <p className="md:text-body-1-normal text-body-2-normal font-semibold text-label-normal">
+              조회수
+            </p>
+            <ChevronIcon className="text-label-assistive" />
+          </span>
+          <hr className="my-3 border-line-normal" />
+
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
+            마켓
+          </span>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {overview?.totalMarketViews.toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              회
+            </span>
+          </p>
+
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
+            콘텐츠
+          </span>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {overview?.totalContentViews.toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              회
+            </span>
+          </p>
+        </Link>
+
+        {/* 카드 4 */}
+        <div className="p-6 rounded-xl bg-background-alternative flex flex-col">
+          <span className="flex justify-between">
+            <p className="md:text-body-1-normal text-body-2-normal font-semibold text-label-normal">
+              고객 수
+            </p>
+            {/* 추후 추가 */}
+            {/* <ChevronIcon className="text-label-assistive" /> */}
+          </span>
+          <hr className="my-3 border-line-normal" />
+
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative">
+            전체
+          </span>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {overview?.totalCustomers.toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              명
+            </span>
+          </p>
+
+          <span className="text-label-1-normal md:text-body-2-normal font-semibold text-label-alternative mt-3">
+            신규
+          </span>
+          <p className="flex text-headline-1 md:text-heading-1 font-bold text-primary-sub-1 items-center md:items-end gap-[0.12rem]">
+            {overview?.recentCustomers.toLocaleString()}
+            <span className="md:text-headline-1 text-body-2-normal font-medium text-primary-sub-1">
+              명
+            </span>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
